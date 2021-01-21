5921c4272177992b054b46e0a5587cd20a5ae2eb
Jorgen <ybeliaev@gmail.com>
2021-01-21 21:44:32 +0200

    css button add

--- src/App.js
+++ src/App.js
@@ -23,7 +23,7 @@ function App() {
       </Flex>
       <Flex direction="column">
           <Console/>
-          <Button outlined align={"flex-end"}>Send</Button>
+          <Button outlined color={'green'} align={"flex-end"}>Send</Button>
       </Flex>
 
     </AppWrapper>
--- src/components/Button.js
+++ src/components/Button.js
@@ -12,6 +12,7 @@ const StyledButton = styled.button`
     &:focus{
         outline: none
     }
+    
     align-self: ${props => props.align || "stretch"};
     ${props => props.primary && css`
             color: ${props=>props.color || "green"};
@@ -21,7 +22,16 @@ const StyledButton = styled.button`
             color: ${props=>props.color || "white"};
             border: 1px solid ${props=>props.color || "white"};
             background-color: transparent;
-    `}
+    `}   
+    box-shadow: inset -1px 0px 5px 0px rgba(0, 100, 0, 0.4), 0px 0px 20px 0px rgba(0, 100, 0, 0.4);
+    transition: all 0.3s;
+    &:hover{    
+       box-shadow: inset -1px 0px 15px 0px rgba(0, 100, 0, 1), 0px 0px 20px 0px rgba(0, 100, 0, 1); 
+    }
+    &:active{    
+       box-shadow: inset -1px 0px 25px -0px rgba(0, 100, 0, 1), 0px 0px 40px 0px rgba(0, 100, 0, 1); 
+       transform: translateY(3px);
+    }
     
 `
 
